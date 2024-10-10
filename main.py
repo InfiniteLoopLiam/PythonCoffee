7e47090fd418bcbf7fd5202488953f4a4fdc19eb
InfiniteLoopLiam <167128941+InfiniteLoopLiam@users.noreply.github.com>
2024-10-10 14:26:18 +0100

    Created function to process coins

--- main.py
+++ main.py
@@ -28,38 +28,56 @@ resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
+    "cost": 0,
 }
+
+
+def insert_coins(money):
+    global resources
+    try:
+        penny = int(input("How many pennies?: ")) * 0.01
+        nickel = int(input("How many nickels?: ")) * 0.05
+        dime = int(input("How many dimes?: ")) * 0.10
+        quarter = int(input("How many quarters?: ")) * 0.25
+    except ValueError:
+        print("Invalid input. Please enter numbers only.")
+        return False
+    total = round(penny + nickel + dime + quarter, 2)
+    if total >= money:
+        resources["cost"] += money
+        change = round(total - money, 2)
+        if change > 0:
+            print(f"here is your change: ${change}")
+        return True
+    else:
+        print(f"Insufficient funds. I have refunded ${total}. Have a nice day!")
+
+
+def can_make_drink(drink):
+    global resources
+    if all(amount <= resources.get(ingredient, 0) for ingredient, amount in MENU[drink]["ingredients"].items()):
+        if insert_coins(MENU[drink]["cost"]):
+            for ingredient, amount in MENU[drink]["ingredients"].items():
+                resources[ingredient] -= amount
+    else:
+        print(f"Sorry, there are not enough resources to make this drink.")
+        return False
+
 
 coffee = True
 while coffee:
-
-    # TODO 1. Prompt user by asking “ What would you like? (espresso/latte/cappuccino): ”
 
     choice = input("What would you like? (espresso/latte/cappuccino): ")
     if choice == "off":
         coffee = False
     elif choice == "report":
         for k, v in resources.items():
-            print(k + ":", v)
-
-    # TODO 4. Check resources sufficient?
-    def can_make_drink(drink):
-        global resources
-        if all(amount <= resources.get(ingredient, 0) for ingredient, amount in MENU[drink]["ingredients"].items()):
-            for ingredient, amount in MENU[drink]["ingredients"].items():
-                resources[ingredient] -= amount
-            print(f"Resources after making {drink}: {resources}")
-            return True
-        else:
-            print("Sorry, there are not enough resources to make this drink.")
-            return False
-
-    print(can_make_drink(choice))
-    print(resources)
-
-
-# TODO 5. Process coins.
-
-# TODO 6. Check transaction successful?
-
-# TODO 7. Make Coffee.
+            if k == "cost":
+                print(f"{k.capitalize()}: ${v}")
+            else:
+                unit = "ml" if k in ("water", "milk") else "g"
+                print(f"{k.capitalize()}: {v}{unit}")
+    elif choice in ("espresso", "latte", "cappuccino"):
+        can_make_drink(choice)
+    else:
+        print("Invalid Input")
